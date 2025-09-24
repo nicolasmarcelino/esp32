15a4ca2f25d3a7c5a90d19da06af402d699e4c89
Nicolas Marcelino <141439592+nicolasmarcelino@users.noreply.github.com>
2025-09-24 18:51:49 -0300

    Update lerSerialChat.ino

--- cibercolab/lerSerialChat.ino
+++ cibercolab/lerSerialChat.ino
@@ -2,22 +2,27 @@
 #define RX_2 16
 
 char incomingByte;
+String remetente;
+String destinatario;
+
 
 void setup() {
-  Serial.begin(9600);                            // USB serial (PC)
-  Serial2.begin(9600, SERIAL_8N1, RX_2, TX_2);   // Serial2 with custom pins
+  Serial.begin(9600);
+  Serial2.begin(9600, SERIAL_8N1, RX_2, TX_2);
 }
 
 void loop() {
-  // Forward from PC to the other ESP
   if (Serial.available() > 0) {
-    incomingByte = Serial.read();
-    Serial2.write(incomingByte);     // send to other ESP
+    //incomingByte = Serial.read();
+    Serial2.print("Nicolas: ");
+    remetente = Serial.readStringUntil('\n');
+    Serial2.println(remetente);
   }
 
-  // Forward from the other ESP to PC
   if (Serial2.available() > 0) {
-    incomingByte = Serial2.read();
-    Serial.write(incomingByte);      // show on PC
+    //incomingByte = Serial2.read();
+    Serial.print("Matheus: ");
+    destinatario = Serial2.readStringUntil('\n');
+    Serial.println(destinatario);
   }
 }
